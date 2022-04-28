07d245eddb5a5591b152a9a6962f071f62cbe4b0
alibaltaci <alibaltaci13@gmail.com>
2022-04-28 23:30:15 +0300

    setNumber

--- Patika.dev-React/07-lifecycle/src/App.js
+++ Patika.dev-React/07-lifecycle/src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 import { useState } from "react"; 
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
+
+      <h1>{number}</h1>
+
+      <button onClick={ () => setNumber(number + 1) }>click</button>
 
     </div>
   );
