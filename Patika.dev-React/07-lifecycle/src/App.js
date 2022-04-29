abcc97a8d03804b566af6f7ccc2e153b60b42eb7
alibaltaci <alibaltaci13@gmail.com>
2022-04-29 22:49:42 +0300

    useEffect Functions

--- Patika.dev-React/07-lifecycle/src/App.js
+++ Patika.dev-React/07-lifecycle/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 
 function App() {
 
   const [number, setNumber] = useState(0);
+
+  const [name, setName] = useState("Ali");
+
+
+  // useEffect
+
+  useEffect( () => console.log("State Güncellendi!"));
+
+  useEffect( () => {
+    console.log("Component Mount Edildi")
+  }, [] )
+  
 
   return (
     <div className="App">
@@ -12,6 +24,12 @@ function App() {
       <h1>{number}</h1>
 
       <button onClick={ () => setNumber(number + 1) }>click</button>
+
+      <br />
+
+      <h1>{name}</h1>
+
+      <button onClick={ () => setName("Ali Baltacı")}>Click</button>
 
     </div>
   );
