1c885c509907bf2d2ffd2773ad28ee8ea10bd1aa
alibaltaci <alibaltaci13@gmail.com>
2022-05-31 23:52:00 +0300

    filter method

--- Patika.dev-React/08-contacts-app/src/components/Contacts/List/index.js
+++ Patika.dev-React/08-contacts-app/src/components/Contacts/List/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import { useState } from "react";
 
 function List( {contacts} ) {
+
+  const [filterText, setFilterText] = useState("");
+
+  // filter metodu
+
+  const filtered = contacts.filter( (item) => {
+    return Object.keys(item).some( (key) => 
+    item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase()));
+  });
+
   return (
     <div>
+
+      <input placeholder='Filter' value={filterText} onChange = { (e) => setFilterText(e.target.value)}  />
+
       <ul>
-        {
+        {/* {
           contacts.map( (contact, i) => (
             <li key={i}> {contact.fullname} </li>
           ))
-        }
+        } */}
+
+        {filtered.map( (contact, i) => 
+          <li key={i}> {contact.fullname} </li>)}
       </ul>
     </div>
   )
