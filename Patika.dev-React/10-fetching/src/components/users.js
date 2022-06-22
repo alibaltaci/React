9b01416b85bfe38c3b186e410251de6f84ed4e42
alibaltaci <alibaltaci13@gmail.com>
2022-06-23 00:21:47 +0300

    request with axios

--- Patika.dev-React/10-fetching/src/components/users.js
+++ Patika.dev-React/10-fetching/src/components/users.js
@@ -1,6 +1,9 @@
 // import useEffect and useState Hooks 
 import {useEffect, useState} from 'react';
 
+// axios 
+import axios from "axios";
+
 function Users() {
 
   // useState   
@@ -11,13 +14,21 @@ function Users() {
 
   useEffect( () => {
 
-    // fetch native
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    // .then(users => console.log(users));
-    .then(users => setUsers(users))
+    // request with fetch native
+
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    // .then(res => res.json())
+    // // .then(users => console.log(users));
+    // .then(users => setUsers(users))
+    // .catch(err => console.log(err))
+    // .finally( () => setIsLoading(false)); // işlem bittikten sonra false olacak. Yukarıda then içinde yapmaktan daha sağlıklı.
+
+    // request with axios 
+
+    axios("https://jsonplaceholder.typicode.com/users")
+    .then(users => setUsers(users.data))  // bize döndüğü objenin altındaki data 'ya erişmemiz lazım.
     .catch(err => console.log(err))
-    .finally( () => setIsLoading(false)); // işlem bittikten sonra false olacak. Yukarıda then içinde yapmaktan daha sağlıklı.
+    .finally( () => setIsLoading(false));
 
 
   }, []);
