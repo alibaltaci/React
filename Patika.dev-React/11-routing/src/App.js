588bbfd5a2980814f58ade420e405aac4d640027
alibaltaci <alibaltaci13@gmail.com>
2022-06-23 23:22:44 +0300

    routing basic path

--- Patika.dev-React/11-routing/src/App.js
+++ Patika.dev-React/11-routing/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+// React Router Lib 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // Basit Bir Örnek
+
+    <BrowserRouter>
+    
+      <Routes>
+
+        <Route path='hi' element={<h1>Hi</h1>} />
+
+        <Route path='Bye' element={<h1>Bye</h1>} />
+
+      </Routes>
+    
+    </BrowserRouter>
+
+    // Daha Gerçekçi Bir Örnek
+    
+
   );
 }
 
