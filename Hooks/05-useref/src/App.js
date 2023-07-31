2bd9d223bf5d6abd1d6577a8b8a9e5aa1055927e
ali baltaci <alibaltaci13@gmail.com>
2023-08-01 01:02:21 +0300

    useRef

---
+++ Hooks/05-useref/src/App.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react';
+
+function App() {
+
+  const inputRef = useRef()
+  const divRef = useRef()
+
+  const [border, setBorder] = useState(false)
+
+  const onBorderDiv = () => {
+    if(!border){
+      divRef.current.style.border = '1px solid blue'
+    }else{
+      divRef.current.style.border = ''
+    }
+
+    setBorder(!border)
+  }
+
+  const onFocusButton = () => {
+    inputRef.current.focus();
+  }
+
+  return ( 
+    <div ref={divRef}>
+      <h1>useRef Kullanım</h1>
+      <input ref={inputRef} placeholder="Giriş..."/>
+      <button onClick={onFocusButton}>Input'a Focus Ol!</button>
+      <button onClick={onBorderDiv}>{ border? "Border'ı kaldır!" : "Border Ekle!"} </button>
+    </div>
+  );
+}
+
+export default App;
