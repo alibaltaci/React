a92513e7ccffa95a6c39177f038170e6a4fbd666
ali baltaci <alibaltaci13@gmail.com>
2023-08-04 16:49:40 +0300

    useInput

--- Hooks/09-custom-hook/src/App.js
+++ Hooks/09-custom-hook/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./styles.css"
+import { useInput } from "./hooks/useInput";
 
 // DRY --> Don't Repeat Yourself.
 
@@ -15,27 +16,43 @@ import "./styles.css"
 
 // Custom Hooklar her kullanıldığında birbirinden ayrı state parçaları olarak depolanır.
 // Yani bir component içinde custom hookumuzu iki kere kullanırsak iki ayrı state parçası oluşturmuş oluruz.
+
 function App() {
 
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [email, setEmail] = useState("")
+  // const [name, setName] = useState("")
+  // const [age, setAge] = useState("")
+  // const [email, setEmail] = useState("")
+
+  //  ===> 
   
-  console.log(name, age, email)
+  // const [inputs, setInputs] = useState( {name:"", age:"", email:""} )
   
+  // stateful logic function
+  // şimdi bu fonksiyonumuzu bir hook olarak tanımlayalım. (hooks --> useInput.jsx)
+
+  // const handleChange = event => {
+  //   setInputs( {...inputs, [event.target.name]: event.target.value} )
+  // }
+
+  // ===> 
+
+  const [inputs, setInputs] = useInput({name:"", age:"", email:""})
+
   return ( 
     <div className="App">
       <h1>Custom Hook Nasıl Yazılır?</h1>
       
       <form>
         <label htmlFor="name">
-          Name: <input name="name" value={name} onChange={ e => setName(e.target.value) } />
+          {/* Name: <input name="name" value={name} onChange={ e => setName(e.target.value) } /> */}
+          {/* Name: <input name="name" value={inputs.name} onChange={ handleChange } /> */}
+          Name: <input name="name" value={inputs.name} onChange={ setInputs } />
         </label>
         <label htmlFor="age">
-          Age: <input name="age" value={age} onChange={ e => setAge(e.target.value) } />
+          Age: <input name="age" value={inputs.age} onChange={ setInputs } />
         </label>
         <label htmlFor="email">
-          Email: <input name="email" value={email} onChange={ e => setEmail(e.target.value) } />
+          Email: <input name="email" value={inputs.email} onChange={ setInputs } />
         </label>
       </form>
     </div>
